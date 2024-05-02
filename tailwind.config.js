ed56407e22cc026812daf140cb459f73d2b5df8e
JulyDR <juliac.kraus@yahoo.de>
2024-05-02 15:07:28 -0400

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ['Figtree', 'sans-serif'],
+      sans: ['Mulish', 'sans-serif'],
       serif: ['Playfair Display', 'serif'],
     },
     extend: {
