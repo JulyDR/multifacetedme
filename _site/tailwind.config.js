1ed920fe1d21b0eeff3dbff688616ee5453af693
JulyDR <juliac.kraus@yahoo.de>
2024-05-08 16:53:34 -0400

    index started

--- _site/tailwind.config.js
+++ _site/tailwind.config.js
@@ -12,9 +12,34 @@ module.exports = {
       serif: ['Playfair Display', 'serif'],
     },
     extend: {
-      
+      spacing: {
+        '5p': '5%',
+        '10p': '10%',
+        '15p': '15%',
+      },
+      aspectRatio: {
+        'landscape': '4 / 3 ',
+        'portrait': '3 / 4',
+        'portrait-tall': '3 / 5',
+      },
+      minHeight:{
+        '100': '100vh',
+        '90': '90vh',
+        '80': '80vh',
+        '70': '70vh',
+        '60': '60vh',
+        '50': '50vh',
+      },
+      colors: {
+        light: '#F2D1C8',
+        medium: '#CCADA1',
+        dark: '#B57C71',
+        accentlight: '#557879',
+        accentdark: '#364448',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
 
